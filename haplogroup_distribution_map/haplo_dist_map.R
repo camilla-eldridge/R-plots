136924df749f2f5970d61969f6a79eacdd007392
Camilla Eldridge <k1116370@kingston.ac.uk>
2020-11-02 18:03:37 +0000

    Update haplo_dist_map.R

--- haplogroup_distribution_map/haplo_dist_map.R
+++ haplogroup_distribution_map/haplo_dist_map.R
@@ -4,9 +4,6 @@ args = commandArgs(trailingOnly=TRUE)
 library(ggplot2)
 library(ggmap)
 
-#require(ggrepel)
-#library("ggsci")
-
 haplotype_csv<-args[1]
 output_plot_name<-args[2]
 
@@ -18,5 +15,5 @@ world_map<-get_stamenmap(bbox = c(left = -20, bottom = 20, right = 170, top = 75
 map<-ggmap(world_map, crs = "EPSG:4326")
 dist_map_d<-map +  geom_point(data = coords, aes(x = Latitude, y = Longitude, color =  Haplogroup ) , shape = 18, size = 4)
 J<-dist_map_d + labs(color = "Haplogroup") + xlab("Longitude") + ylab("Latitude") + theme(legend.key = element_blank()) + theme(legend.text = element_text(face = "italic"))
-
+J
 ggsave(output_plot_name, device = "png", width = 35, height = 10, units = "cm")
