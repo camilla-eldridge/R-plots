a8e87c65fa5e4e739ac8ef7edc9acc3437bdd90c
Camilla Eldridge <k1116370@kingston.ac.uk>
2020-11-02 20:28:51 +0000

    added export.jpg

--- interactive_MDS_plot/interactive_MDS_plot.R
+++ interactive_MDS_plot/interactive_MDS_plot.R
@@ -1,15 +1,10 @@
-#!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
-
 library(pegas)
 library(plotly)
 
 #  Plot Distance matrix of sequences and colour by a group variable
-clustalo_matrix<-args[1]
-labels <-args[2]
 
 # Read distance matrix from Clustalo 
-distance_matrix<-read.csv(clustalo_matrix, row.names = 1) 
+distance_matrix<-read.csv("test.csv", row.names = 1) 
 
 #weighted euclidean distance
 dist_eu <- as.matrix(dist(distance_matrix, method = "euclidean"))
@@ -18,7 +13,7 @@ mds_eu$names <- rownames(mds_eu)
 mds_eu<- mds_eu[order(mds_eu$names),]
 
 #  merge with Species info
-label_table<-read.csv(labels, header = TRUE)
+label_table<-read.csv("test_labels.csv", header = TRUE)
 colnames(mds_eu)<- c("V1", "V2", "Species")
 mds_final<-merge(mds_eu, label_table)
 mds_df<-data.frame(mds_final)
@@ -26,4 +21,4 @@ mds_df<-data.frame(mds_final)
 # Make interactive plot for MDS of genetic distance (raw) and colour points by group
 attach(mds_df)
 mds_plotly<-plot_ly(data = mds_df, x = V1, y = V2, mode = "markers", type='scatter', color = Group, text = Species)
-mds_plotly
+mds_plotly 
